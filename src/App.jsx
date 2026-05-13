01604e403c18b4781871e3dfeccb28ff1715a2c3
Maxwell Vale <max.s.vale01@gmail.com>
2026-05-13 19:05:05 -0400

    Fixed layout of pages in CSS to remove empty side borders and extend header to edges of page

--- src/App.jsx
+++ src/App.jsx
@@ -17,25 +17,27 @@ export default function App() {
     <BrowserRouter>
       <div className="app-shell">
         <header className="app-header">
-          <Link to="/" className="brand">
-            Tray Inventory
-          </Link>
+          <div className="app-header-inner">
+            <Link to="/" className="brand">
+              Tray Inventory
+            </Link>
 
-          <div className="header-actions"> {/* User Login Header */}
-            {authLoading ? (
-              <span>Loading...</span>
-            ) : authUser ? (
-              <>
-                <span className="user-label">{authUser.email}</span>
-                <button className="secondary-button" onClick={handleSignOut}>
-                  Sign Out
-                </button>
-              </>
-            ) : (
-              <Link to="/login" className="button-link">
-                Staff Login 
-              </Link>
-            )}
+            <div className="header-actions"> {/* User Login Header */}
+              {authLoading ? (
+                <span>Loading...</span>
+              ) : authUser ? (
+                <>
+                  <span className="user-label">{authUser.email}</span>
+                  <button className="secondary-button" onClick={handleSignOut}>
+                    Sign Out
+                  </button>
+                </>
+              ) : (
+                <Link to="/login" className="button-link">
+                  Staff Login 
+                </Link>
+              )}
+            </div>
           </div>
         </header>
 
